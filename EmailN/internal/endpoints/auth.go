cefc12cdab4ade47dcfb93606a429743c2179d1c
Ezequiel Menneck <zeeeee.peeeeetryyy@gmail.com>
2024-09-02 18:21:51 -0300

    getting user email by jwt token and changing post method to save email

--- EmailN/internal/endpoints/auth.go
+++ EmailN/internal/endpoints/auth.go
@@ -1,7 +1,9 @@
 package endpoints
 
 import (
-	"github.com/coreos/go-oidc/v3/oidc"
+	"context"
+	oidc "github.com/coreos/go-oidc/v3/oidc"
+	jwtgo "github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/render"
 	"net/http"
 	"strings"
@@ -9,14 +11,14 @@ import (
 
 func Auth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token := r.Header.Get("Authorization")
-		if token == "" {
+		tokenString := r.Header.Get("Authorization")
+		if tokenString == "" {
 			render.Status(r, http.StatusUnauthorized)
 			render.JSON(w, r, map[string]string{"error": "request does not contain an authorization header"})
 			return
 		}
 
-		token = strings.Replace(token, "Bearer ", "", 1)
+		tokenString = strings.Replace(tokenString, "Bearer ", "", 1)
 		provider, err := oidc.NewProvider(r.Context(), "http://localhost:8080/realms/provider")
 		if err != nil {
 			render.Status(r, http.StatusInternalServerError)
@@ -25,12 +27,19 @@ func Auth(next http.Handler) http.Handler {
 		}
 
 		verifier := provider.Verifier(&oidc.Config{ClientID: "emailn"})
-		_, err = verifier.Verify(r.Context(), token)
+		_, err = verifier.Verify(r.Context(), tokenString)
 		if err != nil {
 			render.Status(r, http.StatusUnauthorized)
-			render.JSON(w, r, map[string]string{"error": "invalid token"})
+			render.JSON(w, r, map[string]string{"error": "invalid tokenString"})
 			return
 		}
-		next.ServeHTTP(w, r)
+
+		token, _ := jwtgo.Parse(tokenString, nil)
+		claims := token.Claims.(jwtgo.MapClaims)
+		email := claims["email"]
+
+		ctx := context.WithValue(r.Context(), "email", email)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
--- EmailN/internal/endpoints/campaigns_post.go
+++ EmailN/internal/endpoints/campaigns_post.go
@@ -9,6 +9,8 @@ import (
 func (h *Handler) CampaignPost(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
 	var request contract.NewCampaign
 	err := render.DecodeJSON(r.Body, &request)
+	email := r.Context().Value("email").(string)
+	request.CreatedBy = email
 	if err != nil {
 		return nil, http.StatusBadRequest, err
 	}
